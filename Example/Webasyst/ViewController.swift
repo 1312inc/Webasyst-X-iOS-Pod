0e7896b8980b65c598880afe2d9c5e5cc1611228
viktkobst <viktkobst@gmail.com>
2021-05-12 15:31:54 +0700

    reinit

---
+++ Example/Webasyst/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Webasyst
+//
+//  Created by viktkobst on 05/12/2021.
+//  Copyright (c) 2021 viktkobst. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
---
+++ Webasyst/Classes/TestClass.swift
@@ -0,0 +1,14 @@
+//
+//  TestClass.swift
+//  Pods-Webasyst_Example
+//
+//  Created by Виктор Кобыхно on 12.05.2021.
+//
+
+import Foundation
+
+public class TestClass {
+    public init() {}
+    
+    public let test: String = "test"
+}
