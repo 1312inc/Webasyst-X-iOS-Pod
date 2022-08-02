bdf821a7f26e370577edaa635cd27e850e3ff474
Andrey <64145678+storm67@users.noreply.github.com>
2022-08-02 19:44:40 +0300

    9.1078:new WA cloud account > new account won't open automatically + semi-empty content

--- Source/Structs/CreateNewAccount.swift
+++ Source/Structs/CreateNewAccount.swift
@@ -7,12 +7,12 @@
 
 import Foundation
 
-struct CreateNewAccount: Codable {
-    var id: String
-    var domain: String
-    var url: String
-    var authEndpoint: String
-    var cloudExpireDate: String
+public struct CreateNewAccount: Codable {
+    public var id: String
+    public var domain: String
+    public var url: String
+    public var authEndpoint: String
+    public var cloudExpireDate: String
     
     enum CodingKeys: String, CodingKey {
         case id, domain, url
